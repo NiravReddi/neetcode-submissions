7e1eedc8a678a5968ddbd59fd334221bd88f6ec2
NiravReddi <46846680+NiravReddi@users.noreply.github.com>
2026-06-16 19:23:38 +0530

    Add: sliding-window-maximum - submission-4

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-4.py
@@ -0,0 +1,25 @@
+import bisect
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        dic=defaultdict(int)
+        heap=[]
+        for i in range(k):
+            dic[nums[i]]+=1
+            if dic[nums[i]]==1:
+                bisect.insort(heap,nums[i])
+        res=[heap[-1]]
+        l=0
+        for r in range(k,len(nums)):
+            dic[nums[r]]+=1
+            if dic[nums[r]]==1:
+                bisect.insort(heap,nums[r])
+            dic[nums[l]]-=1
+            if dic[nums[l]]==0 :
+                del dic[nums[l]]
+                
+                heap.remove(nums[l])
+            
+            res.append(heap[-1])
+            l+=1
+        return res
+            
